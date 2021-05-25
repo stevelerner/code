59ef060d4b63be8e8388e771473a2fbbbfa4995f
Ubuntu <ubuntu@ip-172-31-19-193.ec2.internal>
2021-05-25 11:48:14 +0000

    commit

--- fargate-retail-engine/setup-smartagent.sh
+++
@@ -1,4 +0,0 @@
-export OTEL_SERVICE_NAME='db'
-export OTEL_TRACES_EXPORTER='jaeger-thrift-splunk'
-export OTEL_EXPORTER_JAEGER_ENDPOINT='http://${SMART_AGENT}:9080/v1/trace'
-export OTEL_RESOURCE_ATTRIBUTES='deployment.environment=prod'
